b4ef9a8994fd2499d362e192543bc5c2bac61874
Multezem Kedir <kedirmultezem@gmail.com>
2018-01-10 22:59:38 -0800

    Finshed assigment

--- assigment_1.cpp
+++ assigment_1.cpp
@@ -1,12 +1,11 @@
-
-/********************************************************************* 
- * ** Program Filename:  
- * ** Author: 
- * ** Date: 
- * ** Description: 
- * ** Input: 
- * ** Output: 
- * *********************************************************************/
+/*********************************************************
+ **  Program Filename: assigment_1
+ **  Author: Multezem Kedir 
+ **  Date:   Wednesday Jan 10, 2018 22:55:59 PST
+ **  Description: Calling function from cmath library
+ **  Input: cmath library
+ **  Output: Text
+ ******************************************************** */
 
 #include<iostream>
 #include<cmath>
@@ -16,6 +15,17 @@ using namespace std;
 
 int main(){
 	const double PI = 3.14159265;
+	
+	//calling cos form cmath
 	double result = cos(2/3);	
-	cout << "cos(2/3) = " << result << endl;	
-}
+	cout << "cos(2/3) = " << result << endl;
+	
+	//calling sin from cmath
+	result = 2*sin(2/3);
+	cout << "2sin(2/3) = " << result << endl;
+
+	//calling log10 from cmath
+	result = log10 (55);
+	cout << "log 10 (55) = " << result << endl;
+}	
+
