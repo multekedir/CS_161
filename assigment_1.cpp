11c575120794f777346d6697e0b665ead16bb58b
Multezem Kedir <kedirmultezem@gmail.com>
2018-01-10 18:22:39 -0800

    included cmath

---
+++ assigment_1.cpp
@@ -0,0 +1,21 @@
+
+/********************************************************************* 
+ * ** Program Filename:  
+ * ** Author: 
+ * ** Date: 
+ * ** Description: 
+ * ** Input: 
+ * ** Output: 
+ * *********************************************************************/
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+
+int main(){
+	const double PI = 3.14159265;
+	double result = cos(2/3);	
+	cout << "cos(2/3) = " << result << endl;	
+}
